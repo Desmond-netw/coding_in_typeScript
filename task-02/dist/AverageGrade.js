6bf496f2f29249fd3ed7b41d06273c10394e8082
Desmond-netw <dsquare200@gmail.com>
2024-11-03 19:55:43 +0200

    added words count app

--- task-02/dist/AverageGrade.js
+++ task-02/dist/AverageGrade.js
@@ -13,3 +13,11 @@ let sum = Grade.reduce((acc, grade) => acc + grade, 0);
 //to find averag
 let averageNum = sum / Grade.length;
 console.log("Average number is : ", averageNum.toFixed(2));
+//other method
+for (let value of Grade) {
+    sum += value;
+}
+;
+console.log(sum);
+averageNum = sum / Grade.length;
+console.log(averageNum);
